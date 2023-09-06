f1c9201cacc8b231e5a758c8bb96c9a67d1ba0b6
AaronAnablon <anablonaaronp@gmail.com>
2023-09-06 08:56:01 +0800

    fianl for presentation

--- client/src/components/lecturerComponent/LecturerSiderNavbar.js
+++ client/src/components/lecturerComponent/LecturerSiderNavbar.js
@@ -12,17 +12,22 @@ export default () => {
   return (
     <div className='lecturerSiderNavbar'>
       <Link to='/dashboard'>
-        <div className='lecturerSiderNavbar__logo'>
+        <div className={!collapsed ? "logo_size_big" : "logo_size_small"}>
           <Avatar
             shape="square"
-            className='avatar'
+            className={
+              !collapsed ?
+                'studentSiderNavbar_logo_big' : 'studentSiderNavbar_logo'}
             size='large'
             alt='Face In'
+            title='Face in (Student Version)'
             src={APP_LOGO_URL.link}
             onError={(err) => {
               console.log(err);
             }}
           />
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
           <div
             className={
               !collapsed
